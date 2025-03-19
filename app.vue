c6ca546a483685b98f38769f05920d0952b4b2e3
Emory Jackson Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2025-03-19 00:11:27 -0500

    updated project base to assist team [Issue 24]
    - added placeholders for: nav, footer, and index files
    - added pages and components directories
    - updated eslint to warn on single-word components

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-	<body class="flex min-h-screen bg-red-50 px-15">
-		<p class="mx-5 bg-red-500 px-16 text-lime-200">test</p>
-	</body>
+	<Nav />
+	<NuxtPage />
+	<Footer />
 </template>
 
 <script></script>
