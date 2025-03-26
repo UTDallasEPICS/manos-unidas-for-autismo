a512499cc6cc05ad94f9afc4b6a17ef171eb9a7d
nogr8ness <kabir.azad02@gmail.com>
2025-03-26 16:47:49 -0500

    changed form validation to work with new schema

--- server/api/contactForm/form.post.ts
+++ server/api/contactForm/form.post.ts
@@ -14,9 +14,22 @@ const PreferredContact = {
 } as const;
 
 const Status = {
-	NEEDS_PROCESSING: "NEEDS_PROCESSING",
-	NEEDS_SCHEDULING: "NEEDS_SCHEDULING",
-	PROCESSED: "PROCESSED",
+	NEEDS_PROCESSING: "PROCESSING",
+	NEEDS_SCHEDULING: "SCHEDULING",
+	PROCESSED: "COMPLETED",
+} as const;
+
+const Insurance = {
+	SENASA_CONTRIBUTIVO: "SENASA_CONTRIBUTIVO",
+	SENASA_SUBSIDIADO: "SENASA_SUBSIDIADO",
+	ARS_HUMANO: "ARS_HUMANO",
+	MAPFRE: "MAPFRE",
+	LA_MONUMENTAL: "LA_MONUMENTAL",
+	ARS_UNIVERSAL: "ARS_UNIVERSAL",
+	ARS_META_SALUD: "ARS_META_SALUD",
+	ARS_PLAN_SALUD_BANCO_CENTRAL: "ARS_PLAN_SALUD_BANCO_CENTRAL",
+	RENACER: "RENACER",
+	OTRO: "OTRO",
 } as const;
 
 const schema = z.object({
@@ -26,20 +39,26 @@ const schema = z.object({
 	age: z.number(),
 	gender: z.nativeEnum(Gender),
 	dob: z.coerce.date(),
+	nationality: z.string(),
+	streetName: z.string(),
+	streetNum: z.number(),
+	buildingNum: z.number(),
+	postcode: z.number(),
 	parentName: z.string().nullable(),
-	idCard: z.string(),
+	identification: z.string(),
 	birthCountry: z.string(),
 	birthCity: z.string(),
 	nss: z.string(),
-	insurance: z.string(),
+	insurance: z.nativeEnum(Insurance),
 	address: z.string(),
-	preferredContact: z.nativeEnum(PreferredContact),
+	contactPref: z.nativeEnum(PreferredContact),
 	email: z.string().email(),
 	phone: z.string().nullable(),
 	isDiagnosed: z.boolean(),
 	hasBeenPatient: z.boolean(),
 	status: z.nativeEnum(Status),
 	wantsEval: z.boolean(),
+	comment: z.string(),
 });
 
 const validateSchema = schema.strict();
@@ -62,52 +81,69 @@ export default defineEventHandler(async (event) => {
 		lName,
 		gender,
 		dob,
-		nss,
-		idCard,
-		address,
-		preferredContact,
+		nationality,
+		insurance,
+		identification,
+		streetName,
+		streetNum,
+		buildingNum,
+		postcode,
+		contactPref,
 		email,
 		phone,
 		isDiagnosed,
 		hasBeenPatient,
 		status,
 		wantsEval,
+		comment,
 	} = validatedBody.data;
 
 	//Create User entry
 	const user = await prisma.user.create({
 		data: {
-			FName: fName,
-			MInit: mInit,
-			LName: lName,
-			Email: email,
-			Phone: phone,
-			PreferredContact: preferredContact,
+			fName: fName,
+			mInit: mInit,
+			lName: lName,
+			email: email,
+			phone: phone,
+			contactPref: contactPref,
+		},
+	});
+
+	//Create NonEmployee Entry
+	const nonEmployee = await prisma.nonEmployee.create({
+		data: {
+			id: user.id,
+			gender: gender,
+			dob: dob,
+			streetName: streetName,
+			streetNum: streetNum,
+			buildingNum: buildingNum,
+			postCode: postcode,
 		},
 	});
 
 	//Create Patient entry
 	const patient = await prisma.patient.create({
 		data: {
-			ID: user.ID,
-			DOB: dob,
-			Gender: gender,
-			Address: address,
-			NSS: nss,
-			Identification: idCard,
-			Diagnosed: isDiagnosed,
+			id: user.id,
+			identification: identification,
+			diagnosed: isDiagnosed,
 		},
 	});
 
 	//Create ContactForm entry
 	const form = await prisma.contactForm.create({
 		data: {
-			BeenHereBefore: hasBeenPatient,
-			WantsDiagnosticEval: wantsEval,
-			Status: status,
-			patientID: patient.ID,
+			returnPatient: hasBeenPatient,
+			wantsEval: wantsEval,
+			insurance: insurance,
+			status: status,
+			nationality: nationality,
+			comment: comment,
+			patientId: patient.id,
 		},
 	});
 
-	return { user, patient, form };
+	return { user, nonEmployee, patient, form };
 });
