e8d2e14ae1d27193668b958890b6573f30532879
ravia <sreejama33@gmail.com>
2025-03-26 16:12:47 -0500

    more ui changes to contact form

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>This is a Template Index page</h1>
+	<p>It needs liks to login & to submit a contact form</p>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import { navigateTo } from "#imports";
+/* add you page's name after the / in 'navigateTo("/")' */
+onMounted(async () => {
+	await navigateTo("/ContactForm");
+});
+</script>
