6dde6bf958eece3884f74116295824a87431ab4e
Amelie Yu <amelie.m.yu@gmail.com>
2025-04-09 15:52:11 -0500

    Add permissions
    added a temporary object for storing permissions. filter applies to admin & user service. edit appointments applies to user service. it'd be false for both

--- pages/ScheduleView.vue
+++ pages/ScheduleView.vue
@@ -1,26 +1,33 @@
-<!-- 8 Apr 2025
+<!-- 9 Apr 2025
  schedule view page, clicking the buttons changes the week that's being displayed -->
 <template>
 	<div class="m-10">
 		<!-- Title part + option buttons -->
 		<div class="my-5 flex justify-between">
 			<div class="flex flex-col justify-center">
-				<div class="text-xl">Current Schedule for</div>
+				<div class="text-xl">Schedule for</div>
 				<div class="text-3xl">{{ currentWeek }}</div>
 			</div>
-			<div>
-				<createAppointment />
-				<button @click="filterAppointments()">Filter</button>
+			<div class="justify-right flex flex-col gap-2">
+				<createAppointment v-if="permissions.editAppointments" />
+				<!-- placeholder style for the button so it's not just text lmao -->
+				<button
+					@click="filterAppointments()"
+					class="bg-blue-950 p-2 text-white"
+					v-if="permissions.filter"
+				>
+					Filter
+				</button>
 			</div>
 		</div>
 
 		<div class="flex justify-center">
-			<div class="px-2 align-top text-xl">
+			<div class="pr-3 align-top text-3xl">
 				<button @click="changeWeek(false)">&#x25C0;</button>
 			</div>
 
 			<WeekViewCalendar :week="date" />
-			<div class="px-2 align-top text-xl">
+			<div class="pl-3 align-top text-3xl">
 				<button @click="changeWeek(true)">&#x25B6;</button>
 			</div>
 		</div>
@@ -29,6 +36,13 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+
+// will be removed later, for now defines the user's permissions
+const permissions = {
+	filter: true, // user service/admin = true, admin/patient/therapist = false
+	editAppointments: true, // user service = true, admin/patient/therapist = false
+};
+
 const date = ref<Date>();
 date.value = new Date(Date.now());
 
