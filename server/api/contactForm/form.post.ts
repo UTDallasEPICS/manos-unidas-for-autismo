873f9b3310cea1532984d3f920ec7a9dfcdbaf18
nogr8ness <kabir.azad02@gmail.com>
2025-04-09 16:16:46 -0500

    fixed some entries in zod schema. imported enums from prisma. implemented more specific error handling.

--- server/api/contactForm/form.post.ts
+++ server/api/contactForm/form.post.ts
@@ -1,65 +1,38 @@
 import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
+import {
+	PrismaClient,
+	Gender,
+	Status,
+	InsuranceCompany,
+	ContactType,
+} from "@prisma/client";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
 const prisma = new PrismaClient();
-
-const Gender = {
-	MALE: "MALE",
-	FEMALE: "FEMALE",
-	OTHER: "OTHER",
-} as const;
-
-const PreferredContact = {
-	EMAIL: "EMAIL",
-	PHONE: "PHONE",
-} as const;
-
-const Status = {
-	NEEDS_PROCESSING: "PROCESSING",
-	NEEDS_SCHEDULING: "SCHEDULING",
-	PROCESSED: "COMPLETED",
-} as const;
-
-const Insurance = {
-	SENASA_CONTRIBUTIVO: "SENASA_CONTRIBUTIVO",
-	SENASA_SUBSIDIADO: "SENASA_SUBSIDIADO",
-	ARS_HUMANO: "ARS_HUMANO",
-	MAPFRE: "MAPFRE",
-	LA_MONUMENTAL: "LA_MONUMENTAL",
-	ARS_UNIVERSAL: "ARS_UNIVERSAL",
-	ARS_META_SALUD: "ARS_META_SALUD",
-	ARS_PLAN_SALUD_BANCO_CENTRAL: "ARS_PLAN_SALUD_BANCO_CENTRAL",
-	RENACER: "RENACER",
-	OTRO: "OTRO",
-} as const;
 
 const schema = z.object({
 	fName: z.string(),
-	mInit: z.string().max(1).nullable(),
+	mInit: z.string().max(1).optional(),
 	lName: z.string(),
-	age: z.number(),
 	gender: z.nativeEnum(Gender),
 	dob: z.coerce.date(),
 	nationality: z.string(),
 	streetName: z.string(),
 	streetNum: z.number(),
-	buildingNum: z.number().nullable(),
+	buildingNum: z.number().optional(),
 	postcode: z.number(),
-	parentName: z.string().nullable(),
 	identification: z.string(),
-	birthCountry: z.string(),
-	birthCity: z.string(),
-	insurance: z.nativeEnum(Insurance),
-	address: z.string(),
-	contactPref: z.nativeEnum(PreferredContact),
+	city: z.string(),
+	insurance: z.nativeEnum(InsuranceCompany),
+	contactPref: z.nativeEnum(ContactType).default("EMAIL"),
 	email: z.string().email(),
-	phone: z.string().nullable(),
-	whatsapp: z.string().nullable(),
+	phone: z.string().optional(),
+	whatsapp: z.string().optional(),
 	isDiagnosed: z.boolean(),
 	hasBeenPatient: z.boolean(),
 	status: z.nativeEnum(Status),
 	wantsEval: z.boolean(),
-	comment: z.string().nullable(),
+	comment: z.string().optional(),
 });
 
 const validateSchema = schema.strict();
@@ -70,10 +43,11 @@ export default defineEventHandler(async (event) => {
 	);
 
 	if (!validatedBody.success) {
-		const zodError = validatedBody.error?.issues;
+		const zodError = validatedBody.error?.format();
 		throw createError({
 			statusCode: 400,
-			statusMessage: `${zodError}`,
+			statusMessage: "Bad Request Body",
+			data: zodError,
 		});
 	}
 
@@ -84,7 +58,7 @@ export default defineEventHandler(async (event) => {
 		gender,
 		dob,
 		nationality,
-		birthCity,
+		city,
 		insurance,
 		identification,
 		streetName,
@@ -101,79 +75,101 @@ export default defineEventHandler(async (event) => {
 		comment,
 	} = validatedBody.data;
 
-	//Check for zipcode city entry
-	const post = await prisma.postCodeCity.findUnique({
-		where: {
-			postCode: postcode,
-		},
-	});
-
-	//If no postcode, add it to database
-	if (!post) {
-		await prisma.postCodeCity.create({
-			data: {
-				postCode: postcode,
-				city: birthCity,
-			},
-		});
-	}
-
 	try {
-		//Create User entry
-		const user = await prisma.user.create({
-			data: {
-				fName: fName,
-				mInit: mInit,
-				lName: lName,
-				email: email,
-				phone: phone,
-				whatsApp: whatsapp,
-				contactPref: contactPref,
-			},
+		//If postcode does not exist, add it to database
+		await prisma.postCodeCity.upsert({
+			where: { postCode: postcode },
+			update: {},
+			create: { postCode: postcode, city },
 		});
 
-		//Create NonEmployee Entry
-		const nonEmployee = await prisma.nonEmployee.create({
-			data: {
-				id: user.id,
-				gender: gender,
-				dob: dob,
-				streetName: streetName,
-				streetNum: streetNum,
-				buildingNum: buildingNum,
-				postCode: postcode,
-			},
+		const result = await prisma.$transaction(async (tx) => {
+			//Create User entry
+			const user = await tx.user.create({
+				data: {
+					fName: fName,
+					mInit: mInit,
+					lName: lName,
+					email: email,
+					phone: phone,
+					whatsApp: whatsapp,
+					contactPref: contactPref,
+				},
+			});
+
+			//Create NonEmployee Entry
+			const nonEmployee = await tx.nonEmployee.create({
+				data: {
+					id: user.id,
+					gender: gender,
+					dob: dob,
+					streetName: streetName,
+					streetNum: streetNum,
+					buildingNum: buildingNum,
+					postCode: postcode,
+				},
+			});
+
+			//Create Patient entry
+			const patient = await tx.patient.create({
+				data: {
+					id: user.id,
+					identification: identification,
+					diagnosed: isDiagnosed,
+				},
+			});
+
+			//Create ContactForm entry
+			const form = await tx.contactForm.create({
+				data: {
+					returnPatient: hasBeenPatient,
+					wantsEval: wantsEval,
+					insurance: insurance,
+					status: "PROCESSING",
+					nationality: nationality,
+					comment: comment,
+					patientId: patient.id,
+				},
+			});
+
+			return { user, nonEmployee, patient, form };
 		});
 
-		//Create Patient entry
-		const patient = await prisma.patient.create({
-			data: {
-				id: user.id,
-				identification: identification,
-				diagnosed: isDiagnosed,
-			},
-		});
-
-		//Create ContactForm entry
-		const form = await prisma.contactForm.create({
-			data: {
-				returnPatient: hasBeenPatient,
-				wantsEval: wantsEval,
-				insurance: insurance,
-				status: "PROCESSING",
-				nationality: nationality,
-				comment: comment,
-				patientId: patient.id,
-			},
-		});
-
-		return { user, nonEmployee, patient, form };
+		return result;
 	} catch (e) {
 		if (e instanceof PrismaClientKnownRequestError) {
-			throw createError({
-				statusCode: 400,
-				statusMessage: e.message,
-			});
+			switch (String(e.code)) {
+				case "P2002":
+					console.log("uniqueness error");
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Schema Uniqueness Constraint Violated",
+					});
+					break;
+				case "P2005":
+				case "P2006":
+					console.log("bad input");
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Wrong input type",
+					});
+					break;
+				case "P2011":
+				case "P2012":
+				case "P2013":
+					console.log("missing required fields");
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Missing required fields",
+					});
+					break;
+				default:
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Unexpected Prisma error",
+						message: e.message,
+					});
+			}
 		}
 	}
 });
