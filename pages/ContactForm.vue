466e328643818de7af4ab85af95947445615279c
ravia <sreejama33@gmail.com>
2025-04-13 16:40:26 -0500

    made some more changes according to pr #43

--- pages/ContactForm.vue
+++ pages/ContactForm.vue
@@ -26,138 +26,110 @@
     -->
 
 	<!--div container for the whole app-->
-	<div class="flex h-full w-full flex-col">
+	<div class="fullPage">
 		<!--Navigation bar-->
 
 		<!--div container for the contact form-->
-		<div class="flex h-auto place-content-center">
-			<div class="inputsBox w-4/5 text-lg md:w-200">
+		<div class="contactFormPage">
+			<div class="contactFormArea">
 				<!--Title of contact form-->
 				<h1 class="mt-5 text-2xl sm:mr-3">Patient Contact Form</h1>
 
 				<!--div container for the form-->
-				<div class="flex justify-start p-5">
+				<div class="contactFormContent">
 					<!--Form for contact form-->
-					<form
-						@submit.prevent="handleSubmit"
-						class="flex flex-col flex-wrap justify-center bg-white"
-					>
+					<form @submit.prevent="handleSubmit" class="contactForm">
 						<!-- div class for the short answer type of responses-->
-						<div
-							class="justify-space-evenly flex flex-wrap gap-5 py-5"
-						>
+						<div class="freeResponseArea py-5">
 							<!--div class for the name portion (the name components should occupy the same line)-->
-							<div
-								class="inputBoxRow flex-col gap-7 sm:flex-col md:flex-row"
-							>
-								<div
-									class="flex w-50 flex-col sm:w-75 md:w-3/10"
-								>
+							<div class="inputBoxRow">
+								<div class="textLabelAndBox_Size1">
 									<label class="">Patient First name:</label>
 
 									<input
 										type="text"
-										class="bg-color2 w-full"
+										class="contactInput"
 										required
 										v-model="firstName"
 									/>
 								</div>
 
-								<div
-									class="flex w-40 flex-col sm:w-60 md:w-1/4"
-								>
+								<div class="textLabelAndBox_Size2">
 									<label class="">Patient Middle name:</label>
 									<!--takes a string, type/enter box-->
 									<input
 										type="middleName"
-										class="bg-color2"
+										class="contactInput"
 										v-model="middleName"
 									/>
 								</div>
 
-								<div
-									class="flex w-50 flex-col sm:w-75 md:w-3/10"
-								>
+								<div class="textLabelAndBox_Size1">
 									<label class="">Patient Last name:</label>
 									<!--takes a string, type/enter box-->
 									<input
 										type="lastName"
-										class="bg-color2"
+										class="contactInput"
 										required
 										v-model="lastName"
 									/>
 								</div>
 							</div>
 
-							<div
-								class="inputBoxRow flex-col gap-7 sm:flex-col md:flex-row"
-							>
-								<div
-									class="flex w-40 flex-col sm:w-60 md:w-1/4"
-								>
+							<div class="inputBoxRow">
+								<div class="textLabelAndBox_Size2 relative">
 									<label class="">Gender:</label>
 
 									<Listbox
 										v-model="gender"
 										as="div"
-										class="bg-color2 flex flex-col overflow-auto"
+										class="contactFormListbox"
 									>
-										<ListboxButton
-											class="flex cursor-pointer content-start px-2"
-											>{{
-												gender == ""
-													? "Select Gender:"
-													: gender
-											}}</ListboxButton
-										>
-										<ListboxOptions
-											as="div"
-											class="flex flex-col bg-gray-100 px-2 text-center"
-										>
-											<ListboxOption
-												as="div"
-												class="flex align-middle"
-												v-for="(g, index) in genders"
-												:key="index"
-												:value="g"
-												v-slot="{ active, selected }"
+										<div>
+											<ListboxButton
+												class="contactFormListboxButton"
+												>{{
+													gender == ""
+														? "Select Gender:"
+														: gender
+												}}</ListboxButton
 											>
-												<li
-													:class="{
-														'cursor-pointer bg-blue-500 text-white':
-															active,
-														'cursor-pointer bg-gray-100 text-black':
-															!active,
-													}"
+											<ListboxOptions
+												as="div"
+												class="contactFormListboxOptions absolute"
+											>
+												<ListboxOption
+													as="div"
+													class="contactFormListboxOption"
+													v-for="(
+														g, index
+													) in genders"
+													:key="index"
+													:value="g"
 												>
-													<CheckIcon
-														v-show="selected"
-													/>
-													{{ g }}
-												</li>
-											</ListboxOption>
-										</ListboxOptions>
+													<div class="px-5">
+														{{ g }}
+													</div>
+												</ListboxOption>
+											</ListboxOptions>
+										</div>
 									</Listbox>
 								</div>
 
-								<div
-									class="flex w-40 flex-col sm:w-60 md:w-1/4"
-								>
+								<div class="textLabelAndBox_Size2">
 									<label class="">Date of Birth:</label>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="date"
 										required
 										v-model="DOB"
 									/>
 								</div>
 
-								<div
-									class="flex w-50 flex-col sm:w-75 md:w-3/10"
-								>
+								<div class="textLabelAndBox_Size1">
 									<label class="">Nationality:</label>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="nationality"
 										required
 										v-model="nationality"
@@ -165,28 +137,22 @@
 								</div>
 							</div>
 
-							<div
-								class="inputBoxRow flex-col gap-7 sm:flex-col md:flex-row"
-							>
-								<div
-									class="flex w-50 flex-col sm:w-75 md:w-3/10"
-								>
+							<div class="inputBoxRow">
+								<div class="textLabelAndBox_Size1">
 									<label class="">ID Number:</label>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="ID"
 										required
 										v-model="ID"
 									/>
 								</div>
-								<div
-									class="flex w-50 flex-col sm:w-75 md:w-3/10"
-								>
+								<div class="textLabelAndBox_Size1">
 									<label class=""
 										>Social Security Number:</label
 									>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="SSN"
 										required
 										v-model="SSN"
@@ -194,60 +160,52 @@
 								</div>
 							</div>
 
-							<div class="inputBoxRow flex-col gap-1 sm:flex-col">
-								<div
-									class="flex w-55 flex-col sm:w-100 md:w-7/10"
-								>
+							<div class="addressInputBoxRow">
+								<div class="textLabelAndBox_Size3">
 									<label class="">Address Line 1:</label>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="address1"
 										required
 										v-model="address1"
 									/>
 								</div>
 
-								<div
-									class="flex w-55 flex-col sm:w-100 md:w-7/10"
-								>
+								<div class="textLabelAndBox_Size3">
 									<label class="">Address Line 2:</label>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="address2"
 										required
 										v-model="address2"
 									/>
 								</div>
 
+								<!-- couldn't transfer this to css from 
+								 tailwind (there is something wrong with responsive design)-->
 								<div class="flex flex-col md:flex-row md:gap-5">
-									<div
-										class="flex w-30 flex-col sm:w-40 md:w-1/5"
-									>
+									<div class="textLabelAndBox_Size4">
 										<label class="">Postal Code:</label>
 										<input
-											class="bg-color2"
+											class="contactInput"
 											type="postalCode"
 											required
 											v-model="postalCode"
 										/>
 									</div>
-									<div
-										class="flex w-40 flex-col sm:w-60 md:w-1/4"
-									>
+									<div class="textLabelAndBox_Size2">
 										<label class="">Locality:</label>
 										<input
-											class="bg-color2"
+											class="contactInput"
 											type="locality"
 											required
 											v-model="locality"
 										/>
 									</div>
-									<div
-										class="flex w-40 flex-col sm:w-60 md:w-1/4"
-									>
+									<div class="textLabelAndBox_Size2">
 										<label class="">Country:</label>
 										<input
-											class="bg-color2"
+											class="contactInput"
 											type="country"
 											required
 											v-model="country"
@@ -256,26 +214,20 @@
 								</div>
 							</div>
 
-							<div
-								class="inputBoxRow flex-col gap-7 sm:flex-col md:flex-row"
-							>
-								<div
-									class="flex w-50 flex-col sm:w-75 md:w-3/10"
-								>
+							<div class="inputBoxRow">
+								<div class="textLabelAndBox_Size1">
 									<label class="">Parent First Name:</label>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="parentFirstName"
 										required
 										v-model="parentFirstName"
 									/>
 								</div>
-								<div
-									class="flex w-50 flex-col sm:w-75 md:w-3/10"
-								>
+								<div class="textLabelAndBox_Size1">
 									<label class="">Parent Last Name:</label>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="parentLastName"
 										required
 										v-model="parentLastName"
@@ -283,29 +235,23 @@
 								</div>
 							</div>
 
-							<div
-								class="inputBoxRow flex-col gap-7 sm:flex-col md:flex-row"
-							>
-								<div
-									class="flex w-55 flex-col sm:w-100 md:w-7/10"
-								>
+							<div class="inputBoxRow">
+								<div class="textLabelAndBox_Size3">
 									<label class="">Email:</label>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="email"
 										required
 										v-model="email"
 									/>
 								</div>
 
-								<div
-									class="flex w-55 flex-col sm:w-100 md:w-7/10"
-								>
+								<div class="textLabelAndBox_Size3">
 									<label class=""
 										>Phone Number/Whatsapp:</label
 									>
 									<input
-										class="bg-color2"
+										class="contactInput"
 										type="phone"
 										required
 										v-model="phone"
@@ -313,62 +259,47 @@
 								</div>
 							</div>
 
-							<div
-								class="inputBoxRow flex-col gap-7 sm:flex-col md:flex-row"
-							>
-								<div
-									class="flex w-55 flex-col sm:w-100 md:w-7/10"
-								>
+							<div class="inputBoxRow">
+								<div class="textLabelAndBox_Size3 relative">
 									<label class="">Medical Insurance:</label>
 
 									<Listbox
 										v-model="insurance"
 										as="div"
-										class="bg-color2 flex flex-col overflow-auto"
+										class="contactFormListbox"
 									>
-										<ListboxButton
-											class="flex cursor-pointer content-start px-2"
-											>{{
-												insurance == ""
-													? "Select the insurance:"
-													: insurance
-											}}</ListboxButton
-										>
-										<ListboxOptions
-											as="div"
-											class="flex flex-col bg-gray-100 px-2 text-center"
-										>
-											<ListboxOption
-												as="div"
-												class="flex align-middle"
-												v-for="(
-													ins, index
-												) in insuranceOptions"
-												:key="index"
-												:value="ins"
-												v-slot="{ active, selected }"
+										<div>
+											<ListboxButton
+												class="contactFormListboxButton"
+												>{{
+													insurance == ""
+														? "Select the insurance:"
+														: insurance
+												}}</ListboxButton
 											>
-												<li
-													:class="{
-														'cursor-pointer bg-blue-500 text-white':
-															active,
-														'cursor-pointer bg-gray-100 text-black':
-															!active,
-													}"
+											<ListboxOptions
+												as="div"
+												class="contactFormListboxOptions absolute"
+											>
+												<ListboxOption
+													as="div"
+													class="contactFormListboxOption"
+													v-for="(
+														ins, index
+													) in insuranceOptions"
+													:key="index"
+													:value="ins"
 												>
-													<CheckIcon
-														v-show="selected"
-													/>
-													{{ ins }}
-												</li>
-											</ListboxOption>
-										</ListboxOptions>
+													<div class="px-5">
+														{{ ins }}
+													</div>
+												</ListboxOption>
+											</ListboxOptions>
+										</div>
 									</Listbox>
 								</div>
 
-								<div
-									class="flex w-55 flex-col sm:w-100 md:w-7/10"
-								>
+								<div class="textLabelAndBox_Size3 relative">
 									<label class=""
 										>Preferred Services/Therapies:</label
 									>
@@ -376,53 +307,41 @@
 										v-model="therapies"
 										multiple
 										as="div"
-										class="bg-color2 flex flex-col content-start overflow-auto"
+										class="contactFormListbox content-start"
 									>
-										<ListboxButton
-											class="flex cursor-pointer content-start px-2"
-											>{{
-												therapies.length > 0
-													? therapies
-															.map(
-																(therapy) =>
-																	therapy.name
-															)
-															.join(", ")
-													: "Select different therapies:"
-											}}</ListboxButton
-										>
-										<ListboxOptions
-											as="div"
-											class="flex flex-col bg-gray-100 px-2 text-center"
-										>
-											<ul>
-												<ListboxOption
-													as="div"
-													class="flex align-middle"
-													v-for="therapy in therapyOptions"
-													:key="therapy.name"
-													:value="therapy"
-													v-slot="{
-														active,
-														selected,
-													}"
-												>
-													<div
-														:class="{
-															'cursor-pointer bg-blue-500 text-white':
-																active,
-															'cursor-pointer bg-gray-100 text-black':
-																!active,
-														}"
+										<div>
+											<ListboxButton
+												class="contactFormListboxButton"
+												>{{
+													therapies.length > 0
+														? therapies
+																.map(
+																	(therapy) =>
+																		therapy.name
+																)
+																.join(", ")
+														: "Select different therapies:"
+												}}</ListboxButton
+											>
+											<ListboxOptions
+												as="div"
+												class="contactFormListboxOptions absolute"
+											>
+												<ul>
+													<ListboxOption
+														as="div"
+														class="contactFormListboxOption"
+														v-for="therapy in therapyOptions"
+														:key="therapy.name"
+														:value="therapy"
 													>
-														<CheckIcon
-															v-show="selected"
-														/>
-														{{ therapy.name }}
-													</div>
-												</ListboxOption>
-											</ul>
-										</ListboxOptions>
+														<div class="px-5">
+															{{ therapy.name }}
+														</div>
+													</ListboxOption>
+												</ul>
+											</ListboxOptions>
+										</div>
 									</Listbox>
 								</div>
 							</div>
@@ -433,9 +352,7 @@
 								<!--only takes 1 doc... don't even know if it transfers to database ;;-->
 								<p>Please sumbit any medical records.</p>
 								<label class="">
-									<button
-										class="manosSubmit flex w-55 flex-col sm:w-100 md:w-35/100"
-									>
+									<button class="fileSubmit">
 										<input
 											@change="registerMedRec"
 											type="file"
@@ -517,12 +434,12 @@
 								</div>
 							</div>
 
-							<div class="flex w-55 flex-col sm:w-100 md:w-7/10">
+							<div class="textLabelAndBox_Size3">
 								<label class=""
 									>Any Comments for the therapist?</label
 								>
 								<textarea
-									class="bg-color2"
+									class="bg-color2 px-2"
 									type="comments"
 									required
 									v-model="comments"
