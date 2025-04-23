63d412f67979967595aa1a9a6165ed1ba0804f24
usrname23 <kld220002@utdallas.edu>
2025-04-23 11:21:46 -0500

    added colors to tw.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,21 @@ export default {
 	],
 	theme: {
 		extend: {
-			colors: {},
+			colors: {
+				/* main colors */
+				pearl: "#FFFFFF",
+				smoky: "#D9D9D9",
+				navy: "#06234F",
+				/* extras */
+				"blu-gray": "#B4BBC9",
+				lilac: "#E3CBF8",
+				salmon: "#EEB586",
+				peach: "#FFE1B8",
+				lime: "#DDF492",
+				"sky-blu": "#B8D3FB",
+				buttercup: "#F6E274",
+				sandy: "#ECE4B9",
+			},
 			fontFamily: {
 				serif: ['"Cormorant Garamond"', "serif"],
 				sans: ['"Encode Sans SC"', "sans-serif"],
